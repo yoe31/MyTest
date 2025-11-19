d6a9931d4551b7b6e68e608f0358c131ec5abe28
yoe31 <65219759+yoe31@users.noreply.github.com>
2025-11-19 15:13:28 +0900

    mainviz.cpp 만들기

---
+++ mainviz.cpp
@@ -0,0 +1,54 @@
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <GLFW/glfw3.h>
+
+#include "UI_Common.hpp"
+
+// 각 모듈 함수
+void DrawButtonPanel();
+void DrawUIMain();
+void DrawUISB();
+void DrawUIPB();
+void DrawUIPP();
+
+int main()
+{
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "IMGUI UI", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 150");
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // --- UI 호출 ---
+        DrawButtonPanel();
+        DrawUIMain();
+        DrawUISB();
+        DrawUIPB();
+        DrawUIPP();
+
+        // ----------------
+        ImGui::Render();
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    // 종료
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
